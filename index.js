2ca09e404983bf5c83d37ccf61f236139d01dd19
Jessica Hille <jessicalhille@gmail.com>
2021-12-18 16:08:45 -0600

    added questions and tested

--- index.js
+++ index.js
@@ -1,13 +1,176 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your GitHub username? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('A GitHub username is required.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('An email address is required.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the name of your GitHub repo? (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('A GitHub repo is required for a badge.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('A project title is required.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('A description of your project is required.');
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'deployedConfirm',
+        message: 'Would you like to include a link to the deployed application?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'deployed',
+        message: 'Please input the deployed application link.',
+        when: ({ deployedConfirm }) => deployedConfirm
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the steps that are necessary to install your project. (Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('A description of installation steps is required.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples of your project in use. (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Usage details are required.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please write any tests and examples of how to run them. (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Tests are required.');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please add guidelines for how other developers can contribute. (Required)',
+        validate: contributingInput => {
+            if (contributingInput) {
+                return true;
+            } else {
+                console.log('A description of how developers can contribute is required.');
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmCredits',
+        message: 'Would you like to list any collaborators or third-party assets?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please list any collaborators, links, and third-party assets used.',
+        when: ({ confirmCredits }) => confirmCredits
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please choose a license for your project.',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNULGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License']
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`./dist/${fileName}`, data, err => {
+        if (err) {
+            throw err;
+        } else {
+            console.log('README created successfully!');
+        };
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(generatedREADME => {
+        return writeToFile('README.md', generatedREADME);
+    })
+    .catch(err => {
+        console.log(err);
+    })
